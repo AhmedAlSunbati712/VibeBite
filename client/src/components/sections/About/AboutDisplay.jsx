faabf0a540134a5b149b0fe6766eab6562321257
Ahmed Moussa Judah Mohamed Judah Al Sunbati/ أحمد موسى جوده محمد جوده السنباطي <117127105+AhmedAlSunbati@users.noreply.github.com>
2025-07-27 22:07:14 -0400

    fixing styling issue

--- client/src/components/sections/About/AboutDisplay.jsx
+++ client/src/components/sections/About/AboutDisplay.jsx
@@ -3,9 +3,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import SongCard from "../Moodify/SongCard.jsx"
 
 function AboutDisplay() {
-    var trackInfo = {
+    const trackInfo = {
         artists: [
-            {name: ["Beach House"]}
+            { name: ["Beach House"] }
         ],
         album: {
             images: [{
@@ -16,21 +16,24 @@ function AboutDisplay() {
         external_urls: {
             spotify: "https://open.spotify.com/track/7H0ya83CMmgFcOhw0UB6ow?si=4d88ee003dd74a85"
         }
-    }
+    };
+
     return (
-        <div className="container col-12 col-lg-6 d-flex justify-content-center align-items-center">
-            <div className="row">            <div className="col-md-8">
-            <SongCard trackInfo={trackInfo}/>
-            </div>
-            <div className="container col-12 col-md-4 d-flex flex-column jusityf-content-center align-items-center">
-                <img className="recipe-img mb-2" src="https://img.spoonacular.com/recipes/642281-556x370.jpg" alt="" />
-                <h3 className="recipte-title text-center">Eggplant Caprese Stack Appetizers</h3>
+        <div className="container mt-4">
+            <div className="row">
+                
+                <div className="col-12 col-md-8 mb-4">
+                    <SongCard trackInfo={trackInfo} />
+                </div>
 
+                
+                <div className="col-12 col-md-4 d-flex flex-column align-items-center">
+                    <img className="recipe-img mb-2 img-fluid" src="https://img.spoonacular.com/recipes/642281-556x370.jpg" alt="Eggplant Caprese Stack Appetizers" />
+                    <h3 className="recipe-title text-center">Eggplant Caprese Stack Appetizers</h3>
+                </div>
             </div>
-            </div>
-
         </div>
-    )
+    );
 }
 
 export default AboutDisplay;
